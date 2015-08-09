717d8406a502dec86b5c91cdfaa2c5f844171f98
陈鹏飞 <908630593@qq.com>
2015-08-09 13:02:55 +0800

    第一次项目初始化

---
+++ 签到/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  签到
+//
+//  Created by okay on 15/8/9.
+//  Copyright (c) 2015年 tesla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 签到/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  签到
+//
+//  Created by okay on 15/8/9.
+//  Copyright (c) 2015年 tesla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
